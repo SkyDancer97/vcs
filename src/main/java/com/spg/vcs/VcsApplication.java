48b1dcc1ef88518fb8ef74a264bccab373cec762
Alexander Shukaylo <skydance1997@gmail.com>
2018-07-05 15:15:39 +0300

    Initial

---
+++ src/main/java/com/spg/vcs/VcsApplication.java
@@ -0,0 +1,14 @@
+package com.spg.vcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcsApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(VcsApplication.class, args);
+  }
+}
---
+++ src/test/java/com/spg/vcs/VcsApplicationTests.java
@@ -0,0 +1,18 @@
+package com.spg.vcs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VcsApplicationTests
+{
+
+  @Test
+  public void contextLoads()
+  {
+  }
+
+}
